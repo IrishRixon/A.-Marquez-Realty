9ad7d9c687e83871e1b805a70f03c67ec9956517
Irish Rixon Dela Pena <irishrixon21delapena@outlook.com>
2025-02-22 13:28:17 +0800

    `feat: updated various components and styles for ARealty application`

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Section1Component } from "./section-1/section-1.component";
 import { Section2Component } from "./section-2/section-2.component";
 import { OurServicesSectionComponent } from './our-services-section/our-services-section.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
-  imports: [Section1Component, Section2Component, OurServicesSectionComponent],
+  imports: [Section1Component, Section2Component, OurServicesSectionComponent, TestimonialsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
