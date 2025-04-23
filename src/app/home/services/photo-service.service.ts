462081fbd87ee98674365b3e9908411e4842df4e
Irish Rixon Dela Pena <irishrixon21delapena@outlook.com>
2025-04-23 19:21:35 +0800

    `feat: Add alt attribute to thumbnail images in section-2.component.html`

--- src/app/home/services/photo-service.service.ts
+++ src/app/home/services/photo-service.service.ts
@@ -11,25 +11,25 @@ export class PhotoService {
   photos: Photo[] = [{
     itemImageSrc: '/assets/images/4pics1.webp',
     thumbnailImageSrc: '/assets/images/4pics1.webp',
-    alt: 'desc',
+    alt: 'Montrose Parkview',
     title: 'Montrose Parkview'
   },
   {
     itemImageSrc: '/assets/images/4pics2.webp',
     thumbnailImageSrc: '/assets/images/4pics2.webp',
-    alt: 'desc',
+    alt: 'Chelsea Parkplace Capital Town Pampanga',
     title: 'Chelsea Parkplace Capital Town Pampanga'
   },
   {
     itemImageSrc: '/assets/images/4pics3.webp',
     thumbnailImageSrc: '/assets/images/4pics3.webp',
-    alt: 'desc',
+    alt: 'Bryant Parklane Capital Town Pampanga',
     title: 'Bryant Parklane Capital Town Pampanga'
   },
   {
     itemImageSrc: '/assets/images/4pics4.webp',
     thumbnailImageSrc: '/assets/images/4pics4.webp',
-    alt: 'desc',
+    alt: 'Shophouse District',
     title: 'Shophouse District'
   }
 ]
